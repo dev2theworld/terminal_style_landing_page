7f7e61b85850c2ef204f58dc845558a424f6654e
Michał Wilk <dev2theworld@gmail.com>
2021-07-05 22:53:03 +0200

    JS - WinBox -> contact window added, background-color setting functions added

--- js/main.js
+++ js/main.js
@@ -6,11 +6,34 @@ const contactContent = document.querySelector('#contact-content');
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
     title: 'About Me',
-    background: '#00aa00',
+    // modal: true,
     width: '450px',
     height: '450px',
     top: '50px',
     left: '50px',
     mount: aboutContent,
+    onfocus: function() {
+      this.setBackground('#00aa00')
+    },
+    onblur: function() {
+      this.setBackground('#777')
+    },
+  });
+})
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    width: '450px',
+    height: '450px',
+    top: '200px',
+    left: '200px',
+    mount: contactContent,
+    onfocus: function() {
+      this.setBackground('#00aa00')
+    },
+    onblur: function() {
+      this.setBackground('#777')
+    },
   });
 })
