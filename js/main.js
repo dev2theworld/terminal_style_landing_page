743fa9abf215114a72eccffd8b9827184a87ec51
Michał Wilk <dev2theworld@gmail.com>
2021-07-13 21:59:32 +0200

    JS - WinBox -> projects window added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const about = document.querySelector('#about');
+const projects = document.querySelector('#projects');
 const contact = document.querySelector('#contact');
 const aboutContent = document.querySelector('#about-content');
+const projectsContent = document.querySelector('#projects-content');
 const contactContent = document.querySelector('#contact-content');
 
 about.addEventListener('click', () => {
@@ -21,13 +23,31 @@ about.addEventListener('click', () => {
   });
 })
 
+projects.addEventListener('click', () => {
+  const projectsBox = new WinBox({
+    title: 'My Projects',
+    modal: true,
+    width: '500px',
+    height: '500px',
+    top: '50px',
+    left: '50px',
+    mount: projectsContent,
+    onfocus: function() {
+      this.setBackground('#00aa00')
+    },
+    onblur: function() {
+      this.setBackground('#777')
+    },
+  });
+})
+
 contact.addEventListener('click', () => {
   const contactBox = new WinBox({
     title: 'Contact Me',
     width: '450px',
     height: '450px',
-    top: '200px',
-    left: '200px',
+    top: '50px',
+    left: '1000px',
     mount: contactContent,
     onfocus: function() {
       this.setBackground('#00aa00')
